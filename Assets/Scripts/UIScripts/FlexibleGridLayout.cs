9eab9cf6e8e2f095297fab18a472023aeb669155
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-07-23 21:59:10 +0100

    grid layout script

--- Assets/Scripts/UIScripts/FlexibleGridLayout.cs
+++ Assets/Scripts/UIScripts/FlexibleGridLayout.cs
@@ -17,22 +17,29 @@ public class FlexibleGridLayout : LayoutGroup
         float SquareRoot = Mathf.Sqrt(transform.childCount);
         rows = Mathf.CeilToInt(SquareRoot);
         columns = Mathf.CeilToInt(SquareRoot);
+
         float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
+
         float cellWidth = parentWidth / (float)columns - ((Spacing.x / (float)columns) * 2);
-        float cellHeight = parentHeight / (float)rows - ((Spacing.y / (float)rows) * 2); ;
+        float cellHeight = parentHeight / (float)rows - ((Spacing.y / (float)rows) * 2);
+
         CellSize.x = cellWidth;
         CellSize.y = cellHeight;
 
         int columnCount = 0;
         int rowCount = 0;
+
         for (int i = 0; i < rectChildren.Count; i++)
         {
             rowCount = i / columns;
             columnCount = i % columns;
+
             var item = rectChildren[i];
-            var xPos = (CellSize.x * columnCount);
-            var yPos = (CellSize.y * rowCount);
+
+            var xPos = (CellSize.x * columnCount) + (Spacing.x * columnCount);
+            var yPos = (CellSize.y * rowCount) + (Spacing.y * rowCount);
+
             SetChildAlongAxis(item, 0, xPos, CellSize.x);
             SetChildAlongAxis(item, 1, yPos, CellSize.y);
         }
