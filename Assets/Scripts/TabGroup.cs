57b6792068312f964a322987e8141f9ade34b670
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-07-23 15:50:11 +0100

    tabs stay selected when clicked

--- Assets/Scripts/TabGroup.cs
+++ Assets/Scripts/TabGroup.cs
@@ -11,6 +11,9 @@ public class TabGroup : MonoBehaviour
     public Sprite HoverTab;
     public Sprite ActiveTab;
 
+    //setting active tab
+    public TabButton SelectedTab;
+
     public void Subscribe(TabButton button)
     {
         if (TabButtons == null)
@@ -25,7 +28,10 @@ public class TabGroup : MonoBehaviour
     {
         ResetTabs();
         //this sprite needs to be "idle" 
-        button.Background.sprite = ActiveTab;
+        if (SelectedTab == null || button != SelectedTab)
+        {
+            button.Background.sprite = ActiveTab;
+        }
     }
 
     public void OnTabExit(TabButton button)
@@ -35,6 +41,7 @@ public class TabGroup : MonoBehaviour
 
     public void OnTabSelected(TabButton button)
     {
+        SelectedTab = button;
         ResetTabs();
         button.Background.sprite = ActiveTab;
     }
@@ -43,6 +50,7 @@ public class TabGroup : MonoBehaviour
     {
         foreach (TabButton button in TabButtons)
         {
+            if (SelectedTab != null && button == SelectedTab) { continue; }
             button.Background.sprite = IdleTab;
         }
     }
