7c11738371e4f0dfbac5b1d3f74990a54a08e745
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-08-27 12:08:25 +0100

    UI changes, settings menu, hotbar and weapon selection menu

--- Assets/Scripts/GameScripts/SettingsMenu.cs
+++ Assets/Scripts/GameScripts/SettingsMenu.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+
 public class SettingsMenu : MonoBehaviour
 {
+    //audio
     public AudioMixer MusicMixer;
     public AudioMixer SFXMixer;
+
+
+    //display
+    public GameObject FPSCounter;
+
+
+    //controlls
+
+
+
+    public void HideFPSCounter(bool Value)
+    {
+        if (Value)
+        {
+            FPSCounter.SetActive(true);
+        }
+        else
+        {
+            FPSCounter.SetActive(false);
+        }
+    }
+
+
+    public void SetSensitivity(float Sens)
+    {
+        float XSens = Sens;
+        float YSens = Sens;
+        FindObjectOfType<PlayerCam>().RecieveSens(XSens, YSens);
+    }
+
     public void SFXVolume(float SFXVol)
     {
         SFXMixer.SetFloat("SFXVolume", SFXVol);
