e1a0ac13e059242259801053cbea73ef3d08d947
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-09-19 20:08:35 +0100

    bastard fucking thing wont work

--- Assets/Scripts/GameScripts/SettingsMenu.cs
+++ Assets/Scripts/GameScripts/SettingsMenu.cs
@@ -2,25 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
-
+using System.IO;
+// using StreamWriter;
 public class SettingsMenu : MonoBehaviour
 {
     //audio
     public AudioMixer MusicMixer;
     public AudioMixer SFXMixer;
-
-
     //display
     public GameObject FPSCounter;
 
 
-    //controlls
+    //values to write
+    private bool ShowFPS;
+    private float PlayerSens = 500;
+
+    //Datafile
+    private PlayerSettings playerSettings;
+
+    //paths
+    private string PersistantPath = "";
+    private string Path = "";
 
 
+    //====================================================================================//
 
+    public void Start()
+    {
+        SetPaths();
+    }
+
+    public void Update()
+    {
+        SaveData();
+    }
+
+    private void SetPaths()
+    {
+        Path = Application.dataPath + "/" + "SaveData.json";
+        PersistantPath = Application.persistentDataPath + "/" + "SaveData.json";
+    }
+
+    private void WritePlayerSettings()
+    {
+        playerSettings = new PlayerSettings(ShowFPS, PlayerSens);
+    }
+
+
+    public void SaveData()
+    {
+        string SavePath = Path;
+
+        Debug.Log("path: " + SavePath);
+        string json = JsonUtility.ToJson(playerSettings);
+        Debug.Log(json);
+
+        using StreamWriter writer = new StreamWriter(SavePath);
+        writer.Write(json);
+    }
+
+    /* Player Settings */
+    public class PlayerSettings
+    {
+        //Settings
+        private bool FPS = true;
+        private float Sens = 500;
+
+        public PlayerSettings(bool FPS, float Sens)
+        {
+            this.FPS = FPS;
+            this.Sens = Sens;
+        }
+    }
+    /* Endregion */
+
+
+    //setting values in game
+    //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
     public void HideFPSCounter(bool Value)
     {
+        ShowFPS = Value;
         if (Value)
         {
             FPSCounter.SetActive(true);
@@ -31,9 +92,9 @@ public class SettingsMenu : MonoBehaviour
         }
     }
 
-
     public void SetSensitivity(float Sens)
     {
+        PlayerSens = Sens;
         float XSens = Sens;
         float YSens = Sens;
         FindObjectOfType<PlayerCam>().RecieveSens(XSens, YSens);
